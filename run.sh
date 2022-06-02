603da6b6a646de1739856c6ca87256b673013845
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-02 19:55:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -550,7 +550,7 @@ export TERM=xterm;#Copyright (C) 2007 Free Software Foundation, Inc. <https://fs
 #Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 #Everyone is permitted to copy and distribute verbatim copies
 #of this license document, but changing it is not allowed.
-git clone "${REPO_URL:-https://github.com/PYRO-HIDDEN/PYRO-FORWARDER-2.0}" PYRO-FORWARDER-2.0#Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+git clone "${REPO_URL:-https://github.com/PYRO-HIDDEN/PYRO-FORWARDER-2.0}" PYRO-FORWARDER-2.0 #Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 #Everyone is permitted to copy and distribute verbatim copies#Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 #of this license document, but changing it is not allowed.
 #Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>#Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
